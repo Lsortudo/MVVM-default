5b53b2c38c676e663f4c0613040694423de32e3d
L <leonardo.alvirubro@hotmail.com>
2023-01-02 20:16:34 -0300

    perf: Repository created to avoid boilerplate

---
+++ app/src/main/java/com/example/mvvmdefault/repositories/MainRepository.kt
@@ -0,0 +1,13 @@
+package com.example.mvvmdefault.repositories
+
+import com.example.mvvmdefault.services.RetrofitService
+
+class MainRepository constructor(private val retrofitService: RetrofitService) {
+// nome é main por conta da MainActivity, caso fosse SignUpActivity seria SignUpRepository
+
+    fun getAllPhotos() = retrofitService.getAllPhotos()
+
+// Motivo da criação de um Repository: Caso mais pra frente vá trocar a biblioteca pra requisição web ou mesmo mockar os dados,
+// só preciso trocar dependencia dentro da função e trocar a chamada na variavel acima após e não alterarei em mais nada,
+// ao invés de percorrer todos arquivos que chamariam o retrofitService.
+}
